5aabef50982b26e289e2d4908cff4f87718ad72c
kennjr <ken.juniour@student.moringaschool.com>
2022-08-13 02:23:39 +0300

    added the id arg to the updated version of the artist, in the edit_artist route

--- app.py
+++ app.py
@@ -322,45 +322,41 @@ def edit_artist(artist_id):
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
-    # TODO: take values from the form submitted, and update existing
+    # ODO: take values from the form submitted, and update existing
     # artist record with ID <artist_id> using the new attributes
-    artist = Artist.query.get(artist_id)
-    if artist:
-        # if we get an item with the id passed int the route then we can make an attempt to process it
-        try:
-            genres = request.form.get('genres')
-            name = request.form.get('name')
-            city = request.form.get('city')
-            state = request.form.get('state')
-            phone = request.form.get('phone')
-            image_link = request.form.get('image_link')
-            facebook_link = request.form.get('facebook_link')
-            website_link = request.form.get('website_link')
-            seeking_venue = request.form.get('seeking_venue')
-            seeking_description = request.form.get('seeking_description')
 
-            # converting the seeking_venue str val to a bool
-            if seeking_venue == 'y':
-                seeking_venue = True
-            else:
-                seeking_venue = False
-            # add the data to the server
-            artist_item = Artist(genres=genres, name=name, city=city, state=state, phone=phone, image_link=image_link,
-                                 facebook_link=facebook_link, website_link=website_link, seeking_venues=seeking_venue,
-                                 seeking_description=seeking_description)
-            db.session.add(artist_item)
-            db.session.commit()
-            # on successful db insert, flash success
-            flash('Artist ' + request.form['name'] + ' was successfully listed!')
-        except:
-            print(sys.exc_info())
-            db.session.rollback()
-            # on successful db insert, flash success
-            flash('An error occurred. Artist ' + request.form.get('name') + ' could not be listed.')
-        finally:
-            db.session.close()
-        return redirect(url_for('index'))
-    else:
+    try:
+        genres = request.form.get('genres')
+        name = request.form.get('name')
+        city = request.form.get('city')
+        state = request.form.get('state')
+        phone = request.form.get('phone')
+        image_link = request.form.get('image_link')
+        facebook_link = request.form.get('facebook_link')
+        website_link = request.form.get('website_link')
+        seeking_venue = request.form.get('seeking_venue')
+        seeking_description = request.form.get('seeking_description')
+
+        # converting the seeking_venue str val to a bool
+        if seeking_venue == 'y':
+            seeking_venue = True
+        else:
+            seeking_venue = False
+        # add the data to the server
+        artist_item = Artist(id=artist_id, genres=genres, name=name, city=city, state=state, phone=phone,
+                             image_link=image_link,
+                             facebook_link=facebook_link, website_link=website_link, seeking_venues=seeking_venue,
+                             seeking_description=seeking_description)
+        db.session.add(artist_item)
+        db.session.commit()
+        # on successful db insert, flash success
+        flash('Artist ' + request.form['name'] + ' was successfully listed!')
+    except:
+        print(sys.exc_info())
+        db.session.rollback()
+        # on successful db insert, flash success
+        flash('An error occurred. Artist ' + request.form.get('name') + ' could not be listed.')
+    finally:
         db.session.close()
     return redirect(url_for('show_artist', artist_id=artist_id))
 
@@ -368,22 +364,14 @@ def edit_artist_submission(artist_id):
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
     form = VenueForm()
-    venue = {
-        "id": 1,
-        "name": "The Musical Hop",
-        "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-        "address": "1015 Folsom Street",
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "123-123-1234",
-        "website": "https://www.themusicalhop.com",
-        "facebook_link": "https://www.facebook.com/TheMusicalHop",
-        "seeking_talent": True,
-        "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-        "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-    }
-    # TODO: populate form with values from venue with ID <venue_id>
-    return render_template('forms/edit_venue.html', form=form, venue=venue)
+    # ODO: populate form with values from venue with ID <venue_id>
+    venue = Venue.query.get(venue_id)
+    if venue:
+        # if we get an item with the id passed int the route then we can make an attempt to process it
+        return render_template('forms/edit_venue.html', form=form, venue=venue)
+    else:
+        flash("The artist doesn't exist")
+        return redirect(url_for('index'))
 
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
