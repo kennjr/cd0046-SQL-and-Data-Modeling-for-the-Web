aaa8dbeadbb5424a492e510e317897e09c56975f
kennjr <ken.juniour@student.moringaschool.com>
2022-08-13 23:28:00 +0300

    created the file and moved the model classes to it

---
+++ models.py
@@ -0,0 +1,48 @@
+from app import db
+
+
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, nullable=False)
+    seeking_description = db.Column(db.String(250))
+    genres = db.Column(db.String(), nullable=False)
+    shows = db.relationship('Show', backref='Venue', cascade="all, delete-orphan", lazy=True)
+
+
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    seeking_description = db.Column(db.String())
+    seeking_venues = db.Column(db.Boolean, nullable=False)
+    shows = db.relationship('Show', backref='Artist', cascade="all, delete-orphan", lazy=True)
+
+
+class Show(db.Model):
+    __tablename__ = "Shows"
+
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+    start_time = db.Column(db.DateTime(timezone=True), nullable=False)
+    artist_image_link = db.Column(db.String(), nullable=True)
+    artist_name = db.Column(db.String())
+    venue_name = db.Column(db.String())
